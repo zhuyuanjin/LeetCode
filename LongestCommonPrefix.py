99f1676a185c489a111bb6925f36c5332417320f
朱垣金 <35217055+zhuyuanjin@users.noreply.github.com>
2018-09-11 09:09:52 +0800

    Update LongestCommonPrefix.py

--- LongestCommonPrefix.py
+++ LongestCommonPrefix.py
@@ -1,19 +1,19 @@
 class Solution:
-    def romanToInt(self, s):
+    def longestCommonPrefix(self, strs):
         """
-        :type s: str
-        :rtype: int
+        :type strs: List[str]
+        :rtype: str
         """
-        
-        dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-        result = 0
-        for i in range(len(s)):
-            if i == len(s) - 1:
-                result += dict[s[i]]
-                continue
-            if dict[s[i]]>=dict[s[i+1]]:
-                result += dict[s[i]]
-            else:
-                result -= dict[s[i]]
-                
+        if len(strs) == 0:
+            return ''
+        result = strs[0]
+        for s in strs:
+            common = ''
+            for i in range(min(len(result), len(s))):
+                if result[i] == s[i]:
+                    common += s[i]
+                else:
+                    break
+            result = common
         return result
+        
